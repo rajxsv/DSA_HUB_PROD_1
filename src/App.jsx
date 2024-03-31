dc2e1a7e94454f1507081c1fde42cd2912a72159
Rajpreet Singh <rajpreetsingh@Rajpreets-MacBook-Pro.local>
2024-03-31 21:01:51 +0530

    Removed signup page redirection to home page on not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import React from "react";
+import { Outlet} from "react-router-dom";
 import SidePanel from "./Pages/SidePanel";
-import { useUser } from "./UserContext";
 
 export default function App() {
-  const { user } = useUser()
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (user == null) {
-      navigate("/signup");
-    }
-  }, []);
-
   return (
     <div className="flex flex-col h-full">
-        <SidePanel />
-        <Outlet />
+      <SidePanel />
+      <Outlet />
     </div>
   );
 }
